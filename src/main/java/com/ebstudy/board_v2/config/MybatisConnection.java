0065c3b173620ad3d0bb6f73b663f5e3fdc03f78
thddntjr1234 <thddntjr112233@gmail.com>
2023-02-15 00:57:23 +0900

    Refactor: SqlSession을 반환하도록 리팩토링
    SqlSessionFactory를 반환해도 openSession()으로 SqlSession을 받아야 각 매퍼 인터페이스를 받을 수 있으므로 이 부분을 통합

--- src/main/java/com/ebstudy/board_v2/config/MybatisConnection.java
+++ src/main/java/com/ebstudy/board_v2/config/MybatisConnection.java
@@ -12,25 +12,29 @@ import java.io.Reader;
 
 public class MybatisConnection {
 
-    private static SqlSessionFactory instance;
+    private static SqlSession session = null;
 
     private MybatisConnection() {}
 
-    public static SqlSessionFactory getInstance() throws IOException {
+    public static SqlSession getSession() throws IOException {
 
         // inputStreamReader는 한글을 읽지 못하므로 Reader 사용
         Reader reader = null;
 
         try {
             String resource = "/mybatis-config.xml";
+
             reader = Resources.getResourceAsReader(resource);
-            instance = new SqlSessionFactoryBuilder().build(reader);
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+
+            session = sqlSessionFactory.openSession();
+
         } catch (Exception e) {
             System.out.println("mybatis-config 설정 실패: " + e);
         } finally {
            try { if(reader != null) reader.close(); } catch (IOException e) {}
         }
 
-        return instance;
+        return session;
     }
 }

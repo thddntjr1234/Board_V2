fe379f259c548429eaf30592a2db18707db8c036
thddntjr1234 <thddntjr112233@gmail.com>
2023-02-16 00:12:11 +0900

    Refactor: JDBC 제거 및 SqlSessionFacoty를 반환하도록 MybatisConnection.java를 리팩토링

--- src/main/java/com/ebstudy/board_v2/config/SessionFactory.java
+++ src/main/java/com/ebstudy/board_v2/config/SessionFactory.java
@@ -1,22 +1,19 @@
 package com.ebstudy.board_v2.config;
 
-import com.ebstudy.board_v2.repository.BoardMapper;
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Reader;
 
-public class MybatisConnection {
+public class SessionFactory {
 
-    private static SqlSession session = null;
+    private static SqlSessionFactory sessionFactory = null;
 
-    private MybatisConnection() {}
+    private SessionFactory() {}
 
-    public static SqlSession getSession() throws IOException {
+    public static SqlSessionFactory getSessionFactory() {
 
         // inputStreamReader는 한글을 읽지 못하므로 Reader 사용
         Reader reader = null;
@@ -27,14 +24,14 @@ public class MybatisConnection {
             reader = Resources.getResourceAsReader(resource);
             SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
 
-            session = sqlSessionFactory.openSession();
-
         } catch (Exception e) {
             System.out.println("mybatis-config 설정 실패: " + e);
         } finally {
-           try { if(reader != null) reader.close(); } catch (IOException e) {}
+           try {
+               if(reader != null) reader.close();
+           } catch (IOException e) {}
         }
 
-        return session;
+        return sessionFactory;
     }
 }
